fe2ffdb4c07394ea9b0db7dd6f838001620d7956
ugurdeniz <ugurdeniz20@gmail.com>
2019-02-04 17:25:52 +0300

    Proje

---
+++ PanteonStrategy/Assets/Scripts/Alert.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Alert : MonoBehaviour
+{
+    public static Alert m;
+    public Text titleText;
+    public Text infoText;
+    public Text buttonText;
+    public Button button;
+    private CanvasGroup canvasGroup;
+
+    void Awake()
+    {
+        m = this;
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    // *** For alert notification screen
+    public void Open(string errorTittle, string errorInfo, string buttonText, UnityAction buttonAction)
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+        titleText.text = errorTittle;
+        infoText.text = errorInfo;
+        this.buttonText.text = buttonText;
+        button.onClick.RemoveAllListeners();
+        if (buttonAction != null)
+            button.onClick.AddListener(buttonAction);
+        button.onClick.AddListener(Close);
+    }
+
+    public void Close()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+}
---
+++ PanteonStrategy/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController m;
+    public Ship selectedShip;
+
+
+    void Awake()
+    {
+        m = this;
+    }
+
+
+//To increase score by coroutine
+    void Start(){
+        StartCoroutine(UpdateEnergyValue());
+    }
+
+    IEnumerator UpdateEnergyValue(){
+        while(true){
+            yield return new WaitForSeconds(1);
+           
+        }
+    }
+}
